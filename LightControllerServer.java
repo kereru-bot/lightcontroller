67c7616cd00cb4c0fae67f7fb069eaba284397d2
ryon <71252225+wyanbot@users.noreply.github.com>
2024-04-06 22:45:27 +1300

    ah

--- LightControllerServer.java
+++ LightControllerServer.java
@@ -17,10 +17,11 @@ class LightControllerServer {
 			int port = Integer.parseInt(args[0]);
 			servSocket = new ServerSocket(port);
 			System.out.println("Listening on port " + port);
-										
+
+
 			//Set up and run process builder to run python script	
-			ProcessBuilder lights = new ProcessBuilder("sudo","python3","tkintertest.py");
-			Process process = lights.start();
+			//ProcessBuilder lights = new ProcessBuilder("sudo","python3","tkintertest.py");
+			//Process process = lights.start();
 			
 		} catch(IOException ex) {
 			System.out.println("IOException " + ex);
@@ -35,18 +36,29 @@ class LightControllerServer {
 				InputStream input = socket.getInputStream();
 				InputStreamReader r = new InputStreamReader(input);
 				BufferedReader reader = new BufferedReader(r);
-				
+				ProcessBuilder builder;
 				while(true) {
 					String command = reader.readLine(); // reads in a command from the client
 					System.out.println(command);
-
+					String[] parts = command.split(" ");
 					//client must've closed connection
 					if(command == null) {
 						break;
 					}
-					if(command.compareTo("activate") == 0) {
-						ProcessBuilder lights = new ProcessBuilder("sudo","python3","test.py");
-						Process process = lights.start();
+
+					if(command.compareTo("on") == 0) {
+						//builder = new ProcessBuilder("sudo","on","test.py");
+
+					} else if(command.compareTo("off") == 0) {
+						builder = new ProcessBuilder("sudo","python3","off.py");
+						builder.start();
+					} else if(command.compareTo("random") == 0) {
+						builder = new ProcessBuilder("sudo","python3","randomlights.py");
+					} else if(parts[0].compareTo("rgb") == 0) {
+						builder = new ProcessBuilder("sudo","python3","on.py",parts[1],parts[2],parts[3]);
+						builder.start();
+					} else if(parts[0].compareTo("brightness") == 0) {
+						//ignore brightness for now
 					}
 				}
 			} catch(IOException ex) {
